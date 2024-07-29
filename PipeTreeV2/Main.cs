202ca0d815edbba053a4ff6cb13b69a35d34b2d1
s.didenko <didenko.sid@yandex.ru>
2024-07-29 21:54:15 +0300

    старт

--- PipeTreeV2/Main.cs
+++ PipeTreeV2/Main.cs
@@ -29,7 +29,89 @@ using Application = Autodesk.Revit.Creation.Application;
 namespace PipeTreeV2
 {
 
-   
+    public class ModelNode
+    {
+        public ElementId ModelElementId { get; set; }
+        public int Counter { get; set; }
+        public List<ElementId> Neighbour { get; set; } = new List<ElementId>();
+
+        public Dictionary<ElementId, List<ElementId>> Connections { get; set; }
+
+        public ModelNode(Autodesk.Revit.DB.Document document, ElementId elementId, int counter)
+        {
+            ModelElementId = elementId;
+            Counter = counter;
+            Element element = document.GetElement(ModelElementId);
+            ConnectorSet connectorSet = GetConnectorSet(element); // Вынесение логики получения ConnectorSet в отдельный метод  
+            if (connectorSet != null)
+            {
+                PopulateVertices(connectorSet); // Вынесение логики заполнения Vertices в отдельный метод 
+                PopulateConnections(connectorSet);
+            }
+        }
+        private ConnectorSet GetConnectorSet(Element element)
+        {
+            if (element is FamilyInstance familyInstance)
+            {
+                return familyInstance.MEPModel?.ConnectorManager.Connectors;
+            }
+            else if (element is Pipe pipe)
+            {
+                return pipe.ConnectorManager.Connectors;
+            }
+
+            return null; // Не найден соответствующий ConnectorSet 
+        }
+
+        private void PopulateVertices(ConnectorSet connectorSet)
+        {
+            foreach (Connector connector in connectorSet)
+            {
+                ConnectorSet nextconnectors = connector.AllRefs;
+                foreach (Connector nextconnector in nextconnectors)
+                {
+                    if (nextconnector.Domain != Domain.DomainUndefined && nextconnector.Owner != null)
+                    {
+                        if (nextconnector.Owner is PipingSystem)
+                        {
+                            continue;
+                        }
+                        else if (!Neighbour.Contains(nextconnector.Owner.Id))
+                        {
+                            if (nextconnector.Direction == FlowDirectionType.Out || nextconnector.Direction == FlowDirectionType.Bidirectional)
+                            {
+                                Neighbour.Add(nextconnector.Owner.Id);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        private void PopulateConnections(ConnectorSet connectorSet)
+        {
+            foreach (Connector connector in connectorSet)
+            {
+                ConnectorSet nextconnectors = connector.AllRefs;
+                foreach (Connector nextconnector in nextconnectors)
+                {
+                    if (nextconnector.Domain != Domain.DomainUndefined && nextconnector.Owner != null)
+                    {
+                        ElementId ownerId = nextconnector.Owner.Id;
+                        if (!Connections.ContainsKey(ownerId))
+                        {
+                            Connections[ownerId] = new List<ElementId>();
+                        }
+                        Connections[ownerId].Add(connector.Owner.Id); // Добавляем связь в словарь
+                    }
+                }
+            }
+        }
+
+        // Вызов нового метода для заполнения Connections
+
+
+    }
 
     
 
@@ -66,7 +148,11 @@ namespace PipeTreeV2
 
         static AddInId AddInId = new AddInId(new Guid("CDFCB89B-70AD-452A-91A7-EB47D70781BF"));
 
-
+        static IList<Element> GetSystems(Autodesk.Revit.DB.Document document)
+        {
+            IList<Element> systems = new FilteredElementCollector(document).OfCategory(BuiltInCategory.OST_PipingSystem).WhereElementIsNotElementType().ToElements();
+            return systems;
+        }
 
 
 
@@ -112,7 +198,32 @@ namespace PipeTreeV2
             window.ShowDialog();
             ///
 
-            
+            var systems = GetSystems(doc);
+            List<Element> selectedsystems = new List<Element>();
+            //Тут фильтруем системы по наличию в имени сокращения
+            foreach (var sys in systems)
+            {
+                if(sys.Name.Contains(dataContext.ContextViewModel.SelectedSystemName))
+                {
+                    selectedsystems.Add(sys);
+                }
+            }
+
+            foreach (var sys in selectedsystems)
+            {
+                List<ModelNode> startconnectors = new List<ModelNode>();
+               var connectors = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MechanicalEquipment).WhereElementIsNotElementType().ToElementIds();
+                int counter = 1;
+                foreach(ElementId element in connectors)
+                {
+                    if (doc.GetElement(element).Name.Contains("Виртуальный коннектор"))
+                    {
+                        ModelNode modelNode = new ModelNode(doc, element, counter);
+                        startconnectors.Add(modelNode);
+                    }    
+                }
+
+            }
                
                
 
